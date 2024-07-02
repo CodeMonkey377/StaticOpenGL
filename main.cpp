4ba76da196572ce88faad2864d2260b57143d1d4
Joseph Davies <josephdavies377@gmail.com>
2024-07-02 09:36:30 -0500

    First save. Add glfw and glad

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "glad/include/glad/glad.h"
+#include "glfw-3.4/include/GLFW/glfw3.h"
+
+
+int main() {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+}
