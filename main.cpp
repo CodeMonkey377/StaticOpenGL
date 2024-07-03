0f17d1a45f39b50fd401400a743ae05c38cad3af
Joseph Davies <josephdavies377@gmail.com>
2024-07-03 16:14:55 -0500

    Created VBO, VAO, vertices and render square

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include "glad/include/glad/glad.h"
 #include "glfw-3.4/include/GLFW/glfw3.h"
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 
+/// Shader file reader function declaration
+std::string readFile(const char* filePath){
+    std::ifstream file(filePath);
+    if(!file.is_open()) {
+        std::cerr << "Failed to open shader file: " << filePath << "\n";
+        exit(EXIT_FAILURE);
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
 
 int main() {
     glfwInit();
@@ -9,21 +26,30 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // Two Triangles that make up square
+    GLfloat vertices[18] = {
+            1.0f, 1.0f, 0.0f,
+            -1.0f, 1.0f, 0.0f,
+            -1.0f, -1.0f, 0.0f,
 
-    struct tile{
-        GLfloat vertices[12] = {
-                -1.0f, -1.0f, 0.0f,
-                1.0f, -1.0f, 0.0f,
-                -1.0f, 1.0f, 0.0f,
-                1.0f, 1.0f, 0.0f,
-        };
-
-
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            1.0f, 1.0f, 0.0f,
     };
 
+    struct tile{
+        // represents corner of tile or maybe offset
+        std::vector<glm::vec2> position;
+
+        /// Value of neighbors, need to figure out how pos will work first
+
+        // will go to fragment shader when drawing I am guessing
+        bool is_on;
+    };
+    int map_width;
+    int map_height;
 
     GLFWwindow* window = glfwCreateWindow(1500, 1500, "Static OpenGL", nullptr, nullptr);
-   
     if (window == nullptr){
         std::cout << "Failed to create GLFW window" << "\n";
         glfwTerminate();
@@ -38,11 +64,58 @@ int main() {
    std::cout << "Loaded Open GL\n";
    glViewport(0, 0, 1500, 1500);
 
+    std::string vertex_shader_code = readFile("../shaders/vertex.glsl");
+    const GLchar *vertex_shader_source = vertex_shader_code.c_str();
+    std::string fragment_shader_code = readFile("../shaders/fragment.glsl");
+    const GLchar *fragment_shader_source = fragment_shader_code.c_str();
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+    glCompileShader(vertex_shader);
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+    glCompileShader(fragment_shader);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    /* tests to see if shader is working properly
+    int success;
+    char infoLog[512];
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader_program, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
+                  infoLog << std::endl;
+    }
+    if(success){
+        std::cout << "shader program successfully compiled";
+    }
+    */
+   GLuint VBO, VAO, EBO;
+   glGenVertexArrays(1, &VAO);
+   glBindVertexArray(VAO);
+   glGenBuffers(1, &VBO);
+   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+   /* not entirely sure we need an EBO yet...
+   glGenBuffers(1, &EBO);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+   */
+   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), nullptr);
+   glEnableVertexAttribArray(0);
+
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 18);
 
         glfwSwapBuffers(window);
     }
---
+++ shaders/fragment.glsl
@@ -0,0 +1,8 @@
+// Fragment Shader Source
+#version 460 core
+out vec4 frag_color;
+
+void main()
+{
+    frag_color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+}
