ad91e982e5c8b3fd5b0cc4d149c3d054d5f9febe
Joseph Davies <josephdavies377@gmail.com>
2024-07-02 20:23:50 -0500

    Created window, loaded GL function pointers and terminate.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include "glad/include/glad/glad.h"
 #include "glfw-3.4/include/GLFW/glfw3.h"
+#include <iostream>
 
 
 int main() {
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    GLFWwindow* window = glfwCreateWindow(1500, 1500, "Static OpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if (window == nullptr){
+        std::cout << "Failed to create GLFW window" << "\n";
+        glfwTerminate();
+        return -1;
+    }
 
+   if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
+       std::cout << "Glad failed to load GL function pointers" << "\n";
+       return -2;
+   }
+
+   glfwTerminate();
+   return 0;
 }
