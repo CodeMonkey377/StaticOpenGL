de36c1d268e60a51d0c11d3ce54f5fb9fefabf3f
Joseph Davies <josephdavies377@gmail.com>
2024-07-13 23:11:59 -0500

    HP laptop debug for fragment shader

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,6 +7,7 @@ flat in int pause_state;
 // input color depending on pos, either white or black.
 void main()
 {
+    /*
     if (tile_is_alive > 0){
         if (pause_state > 0){
             frag_color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
@@ -18,5 +19,8 @@ void main()
     else{
         frag_color = vec4(0.2f, 0.3f, 0.2f, 1.0f);
     }
-
+    */
+    // Encode tile_is_alive into the red channel
+    float r = float(tile_is_alive) / 255.0;
+    frag_color = vec4(r, 0.0, 0.0, 1.0);
 }
